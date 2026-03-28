e94f52fc61723972589143cbb1ea73bb2c8a5d4e
Maria Fernanda Tolosa <fernandaangel242@gmail.com>
2026-03-28 16:44:37 -0500

    Update ventas_cafeteria.py

--- ventas_cafeteria.py
+++ ventas_cafeteria.py
@@ -3,6 +3,20 @@
 #FUNDAMENTOS DE PROGRAMACION
 #CODIGO FUENTE: AUTORIA PROPIA
 
+
+#Una tienda de café desea automatizar el registro de
+#ventas diarias. El administrador registra las ventas en tres momentos:
+#mañana, tarde y noche.
+#El programa debe:
+#1. Solicitar el valor de las ventas en cada momento.
+#2. Calcular el total de ventas del día.
+#3. Comparar el total con la meta diaria de $150.000.
+#Salida esperada:
+#- Total vendido.
+#- Mensaje:
+#✓ “Meta alcanzada” si el total ≥ 150.000.
+#✓ “Meta no alcanzada” si el total < 150.000.
+
 # Meta diaria
 META = 150000
 
@@ -23,3 +37,5 @@ if total >= META:
     print("Meta alcanzada ✅")
 else:
     print("Meta no alcanzada ❌")
+
+#Realizado por Maria Fernanda Tolosa Angel
