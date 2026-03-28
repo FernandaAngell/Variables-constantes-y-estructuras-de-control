dcb3bf5a6945385f239b4dd1dfad2462a0678cf3
Maria Fernanda Tolosa <fernandaangel242@gmail.com>
2026-03-28 16:42:57 -0500

    Update peso_envio_jugueteria.py

--- peso_envio_jugueteria.py
+++ peso_envio_jugueteria.py
@@ -3,6 +3,16 @@
 #FUNDAMENTOS DE PROGRAMACION
 #CODIGO FUENTE: AUTORIA PROPIA
 
+#La juguetería “Mundo Mágico” realiza envíos nacionales
+#en su campaña “Sonrisas por correo”. Para calcular el costo del envío,
+#se necesita conocer el peso total del pedido.
+#Datos:
+#- Cada payaso de tela pesa 112 gramos.
+#- Cada muñeca clásica pesa 75 gramos.
+#El programa debe:
+#- Solicitar la cantidad de payasos y muñecas en el pedido.
+#- Calcular y mostrar el peso total en gramos con un mensaje claro.
+
 # Peso de cada producto (en gramos)
 PESO_PAYASO = 112
 PESO_MUNECA = 75
@@ -19,3 +29,4 @@ print("\n=== RESULTADO DEL ENVÍO ===")
 print(f"Cantidad de payasos: {payasos}")
 print(f"Cantidad de muñecas: {munecas}")
 print(f"Peso total del paquete: {peso_total} gramos")
+#Realizado por Maria Fernanda Tolosa Angel
